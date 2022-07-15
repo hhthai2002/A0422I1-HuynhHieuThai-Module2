1f6c2e579b82c76841ac077929dd1d4d6a366046
Thai Vjp Pro <you@example.com>
2022-07-15 17:33:17 +0700

    up

---
+++ ss2_loop/src/exercise/IntegersShowing.java
@@ -0,0 +1,14 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class IntegersShowing {
+    public static void main(String[] args) {
+        do{
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter amount of number: ");
+            int numbers = sc.nextInt();
+        }
+        while(true);
+    }
+}
