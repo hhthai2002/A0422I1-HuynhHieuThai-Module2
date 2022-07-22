f91c7fe168171667ad1fcbbe467697521a787734
Thai Vjp Pro <you@example.com>
2022-07-22 11:44:47 +0700

    up

---
+++ ss4_class_and_object/src/exercises/Circle.java
@@ -0,0 +1,26 @@
+package exercises;
+
+public class Circle {
+    private static double radius = 1.0;
+    private String color = "red";
+
+    public Circle(){
+    }
+
+    public Circle(double radius, String color){
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public double getArea(){
+        return Math.PI * Math.pow(Circle.radius, 2);
+    }
+}
---
+++ ss4_class_and_object/src/exercises/TestCircle.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        System.out.print("The radius of the circle is: " + c.getRadius());
+        System.out.println();
+        System.out.print("The area of the circle is: " + c.getArea());
+        System.out.println();
+        System.out.print("The color of the circle is: " + c.getColor());
+    }
+}
