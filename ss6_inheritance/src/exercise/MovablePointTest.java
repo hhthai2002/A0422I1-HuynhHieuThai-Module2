851619b62cc357966cd710a3acaebdbb9c87b1a9
Thai Vjp Pro <you@example.com>
2022-07-25 19:46:30 +0700

    updating PointMovable

--- ss6_inheritance/src/exercise/MovablePointTest.java
+++ ss6_inheritance/src/exercise/MovablePointTest.java
@@ -3,6 +3,7 @@ package exercise;
 public class MovablePointTest {
     public static void main(String[] args) {
         MovablePoint m = new MovablePoint(1, 2, 3, 4);
+        m.move();
         System.out.println(m);
     }
 }
--- ss6_inheritance/src/exercise/Triangle.java
+++ ss6_inheritance/src/exercise/Triangle.java
@@ -1,4 +1,21 @@
 package exercise;
 
-public class Triangle {
+public class Triangle extends Shape{
+    public Triangle(){
+    }
+    public Triangle(double side1, double side2, double side3, String color){
+        super(side1, side2, side3, color);
+    }
+    public double getPerimeter(){
+        return getSide1() + getSide2() + getSide3();
+    }
+    public double getArea(){
+        double hp = getPerimeter() / 2;
+        return Math.sqrt(hp * (hp - getSide1()) * (hp - getSide2()) * (hp - getSide3()));
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
